99d377aa700d3d8b93d666b3e3b6d89d5fc781f5
coder136016 <avinashkumar00700@gmail.com>
2023-08-08 00:03:21 +0530

    Add project files.

---
+++ dBConnectorApp/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using dBConnectorApp.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using dBConnectorApp.Services;
+
+namespace dBConnectorApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product>? Products;
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products = productService.GetProducts();
+
+        }
+    }
+}
